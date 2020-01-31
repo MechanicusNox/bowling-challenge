a05c58cdf8e7544a0f53164e6f3f47e2c5b3ec9c
Alex Tucker <alextucker@alexs-air.home>
2020-01-31 12:17:38 +0000

    Scoring tests

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -17,5 +17,25 @@ describe("A game", function() {
     expect(game.frames[0][0]).toEqual(3);
   });
 
-  // describe("");
+  describe("can calculate", function() {
+    it('the points for a normal frame', function() {
+      game.inputRoll(3);
+      game.inputRoll(3);
+      expect(game.score).toEqual(6)
+    });
+
+    it('the bonus points for a spare', function() {
+      game.inputRoll(8);
+      game.inputRoll(2);
+      game.inputRoll(2);
+      expect(game.score).toEqual(14)
+    });
+
+    it('the bonus points for a strike', function() {
+      game.inputRoll(10);
+      game.inputRoll(2);
+      game.inputRoll(2);
+      expect(game.score).toEqual(18)
+    });
+  });
 });
