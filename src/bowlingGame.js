f1002d4dc6b44a08566aae714f5b60b74b96834d
Alex Tucker <alextucker@alexs-air.home>
2020-01-31 12:00:25 +0000

    Passed second test

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -3,5 +3,18 @@
 class BowlingGame {
   constructor() {
     this.frames = new Array(10);
+    for (var i = 0; i < 10; i++) {
+      this.frames[i] = new Array(2);
+    }
+    this.currentFrame = 0;
+  };
+
+  inputRoll(score) {
+    if (this.frames[this.currentFrame][0] == null) {
+      this.frames[this.currentFrame][0] = score;
+    } else {
+      this.frames[this.currentFrame][1] = score;
+      this.currentFrame++;
+    }
   };
 };
